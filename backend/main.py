c4e318a615251f1579cba32e369d7d9fce41eada
leozzmc <zzmczzmczzmc870125@gmail.com>
2022-09-26 20:43:52 +0800

    FastAPI & React1 : Creating Models with SQLAlchemy

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-from email import header
 import json
 import string
 import requests
@@ -7,11 +6,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-# APIKEY= "62d08a952c7f8f4363cb0b55bdfd43830cb1a851205de2ca7a107bbf3c21cdbd"
 
-# ----------- FAST API -----------------
+APIKEY= "62d08a952c7f8f4363cb0b55bdfd43830cb1a851205de2ca7a107bbf3c21cdbd"
+
 app = FastAPI()
-# -------- Fix cross origin problems -------
+
 origins = [
     "http://localhost",
     "http://localhost:3000",
@@ -28,43 +27,40 @@ app.add_middleware(
 # class URL(BaseModel):
 #     scan_url: str
 
-# @app.get("/")
-
-# def getScanResult(input: URL):
-    
-#     input = URL(
-#         scan_url='www.google.com'
-#     )
-#     id = base64.urlsafe_b64encode(input.scan_url.encode()).decode().strip("=")
-#     print("-----------------Report------------")
-#     url_4Report = f"https://www.virustotal.com/api/v3/urls/{id}"
-#     header_4Report = {"accept": "application/json","x-apikey":APIKEY}
-#     response = requests.get(url=url_4Report, headers=header_4Report)
-#     print(response.text)
-#     return {"Method":"GET","status": "SUCCESS", "data":response.text}
-
-
-
-# @app.post("/postURL")
-# def scanURL(input: URL):
-#     url = "https://www.virustotal.com/api/v3/urls"
-#     payload = f"url={input.scan_url}"
-#     headers = {
-#     "accept": "application/json",
-#     "content-type": "application/x-www-form-urlencoded",
-#     "x-apikey":APIKEY
-#     }
-#     response = requests.post(url,data=payload, headers=headers)
-#     print(response.text)
-#     return {"Method":"POST","status": "SUCCESS", "data":input}
-
 @app.get("/")
-def get_memes():
-    url = "https://api.imgflip.com/get_memes"
-    header = {"accept": "application/json"}
-    response = requests.get(url=url, headers=header)
+
+def getScanResult():
+    
+    id = base64.urlsafe_b64encode('www.google.com'.encode()).decode().strip("=")
+    print("-----------------Report------------")
+    url_4Report = f"https://www.virustotal.com/api/v3/urls/{id}"
+    header_4Report = {"accept": "application/json","x-apikey":APIKEY}
+    response = requests.get(url=url_4Report, headers=header_4Report)
     print(response.text)
     return {"Method":"GET","status": "SUCCESS", "data":response.text}
+
+
+
+@app.post("/postURL")
+def scanURL():
+    url = "https://www.virustotal.com/api/v3/urls"
+    payload = f"url={input.scan_url}"
+    headers = {
+    "accept": "application/json",
+    "content-type": "application/x-www-form-urlencoded",
+    "x-apikey":APIKEY
+    }
+    response = requests.post(url,data=payload, headers=headers)
+    print(response.text)
+    return {"Method":"POST","status": "SUCCESS", "data":input}
+
+# @app.get("/")
+# def get_memes():
+#     url = "https://api.imgflip.com/get_memes"
+#     header = {"accept": "application/json"}
+#     response = requests.get(url=url, headers=header)
+#     print(response.text)
+#     return {"Method":"GET","status": "SUCCESS", "data":response.text}
 
 
 
--- frontend/src/Scan.js
+++ frontend/src/Scan.js
@@ -1,25 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Slider from "react-slick";
 
 let url = 'http://127.0.0.1:8000'
 
 
 
+
+
+
+    
+    // return (
+    //   <div>
+    //       <h1>Meme Catcher</h1>
+    //       <button onClick={getData(url)}>getData</button>
+    //   </div>
+    // )
+
+
 const Scan = ()=> {
-    function getData(url) {
-        fetch(url, {
-            method: 'GET',
-            redirect: 'follow',
-        })
-            .then((response) =>  console.log(response))
-            .then((result) => console.log(result))
-            .catch((error) => console.log('error', error))
-    }
+  function getData(url) {
+    fetch(url, {
+        method: 'GET',
+        redirect: 'follow',
+    })
+        .then((response) =>  console.log(response))
+        .then((result) => console.log(result))
+        .catch((error) => console.log('error', error))
+  }
+  
+  class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
     return (
       <div>
-          <h1>Meme Catcher</h1>
-          <button onClick={getData(url)}>getData</button>
+        <h1>Meme Catcher</h1>
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+            <button onClick={getData(url)}>getData</button>
+          </div>
+          <div>
+            <h3>2</h3>
+            <button onClick={getData(url)}>getData</button>
+          </div>
+          <div>
+            <h3>3</h3>
+            <button onClick={getData(url)}>getData</button>
+          </div>
+          <div>
+            <h3>4</h3>
+            <button onClick={getData(url)}>getData</button>
+          </div>
+          <div>
+            <h3>5</h3>
+            <button onClick={getData(url)}>getData</button>
+          </div>
+          <div>
+            <h3>6</h3>
+            <button onClick={getData(url)}>getData</button>
+          </div>
+        </Slider>
       </div>
-    )
+    );
+  }
 }
-
+}
 export default Scan;
